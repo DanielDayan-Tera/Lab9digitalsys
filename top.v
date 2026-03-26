0f5de38f400ae9bcd25300fcdf7daf654831937b
DanielDayan-Tera <danieldayan@u.boisestate.edu>
2026-03-26 09:47:46 -0600

    Update top.v

--- top.v
+++ top.v
@@ -1,1 +1,22 @@
 
+module top(
+    input [15:0] sw,     
+    input btnC,          
+    output [15:0] led   
+);
+  
+  d_latch single_bit_memory (
+        .D(sw[0]),
+        .E(btnC),
+        .Q(led[0]),
+        .NotQ(led[1])
+    );
+
+  memory_system four_byte (
+      .data_in(sw[15:8]),
+      .address(sw[7:6]),
+      .store_btn(btnC),
+      .data_out(led[15:8])
+  );
+
+endmodule
